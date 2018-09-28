90939290e0a4800ad4555b039dce8b54f0c5f6ff
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-28 19:24:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese|
+  cheese_types.find do |cheese|
     array.find do |food|
       if cheese == food
         cheese
